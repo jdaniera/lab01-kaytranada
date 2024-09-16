e83db4164f8949a543a3a78c888403d59fab0543
jdaniera <jasminejasmine14@hotmail.com>
2024-09-16 02:01:29 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+
+function App() {
+  
+  return (
+    <>
+      <div className="app">
+        <section id="content">
+          <header>
+            <div>
+              <h1>MyTunes</h1>
+            </div>
+          </header>
+        
+        <div>
+          <h2>KAYTRANADA</h2>
+        </div>
+        <div>
+          img
+        </div>
+        
+        <div>
+          <div>
+            <details>
+              <summary>TIMELESS (2024)</summary>
+              <ol>
+                <li>“Pressure”</li>
+                <li>“Spit It Out” (feat. Rochelle Jordan)</li>
+                <li>“Call U Up” (feat. Lou Phelps)</li>
+                <li>“Weird” (feat. Durand Bernarr)</li>
+                <li>“Dance Dance Dance Dance”</li>
+                <li>“Feel a Way” (feat. Don Toliver)</li>
+                <li>“Still” (feat. Charlotte Day Wilson)</li>
+                <li>“Video” (feat. Ravyn Lenae)</li>
+                <li>“Seemingly”</li>
+                <li>“Drip Sweat” (feat. Channel Tres)</li>
+                <li>“Hold On” (feat. Dawn Richard)</li>
+                <li>“Please Babe”</li>
+                <li>“Stepped On”</li>
+                <li>“More Than a Little Bit” (feat. Tinashe)</li>
+                <li>“Do 2 Me” (feat. Anderson .Paak & SiR)</li>
+                <li>“Witchy” (feat. Childish Gambino)</li>
+                <li>“Lover”</li>
+              </ol>
+            </details>
+          </div>
+
+          <details>
+            <summary>BUBBA (2019)</summary>
+            <ol>
+              <li>“Intro”</li>
+              <li>“DO IT LIKE YOU” (feat. H.E.R.)</li>
+              <li>“PUSHIN ON” (feat. BADBADNOTGOOD)</li>
+              <li>“10%” (feat. Kali Uchis)</li>
+              <li>“Taste” (feat. Masego)</li>
+              <li>“Scared to Death”</li>
+              <li>“Vex Oh” (feat. Charlotte Day Wilson)</li>
+              <li>“SENSITIVE” (feat. Mac DeMarco)</li>
+              <li>“Go DJ” (feat. Teedra Moses)</li>
+              <li>“Culture” (feat. GoldLink)</li>
+              <li>“Midsection” (feat. SiR)</li>
+              <li>“The Worst”</li>
+              <li>“Not PLaying” (feat. BADBADNOTGOOD)</li>
+              <li>“Mold”</li>
+              <li>“Hesitate” (feat. Phonte)</li>
+              <li>“BUBBA”</li>
+            </ol>
+          </details>
+
+          <details>
+            <summary>99.9% (2016)</summary>
+            <ol>
+              <li>“Track Uno”</li>
+              <li>“Bus Ride” (feat. Karriem Riggins & River Tiber)</li>
+              <li>“Got It Good” (feat. Craig David)</li>
+              <li>“Together” (feat. AlunaGeorge & GoldLink)</li>
+              <li>“Drive Me Crazy” (feat. Vic Mensa)</li>
+              <li>“Weight Off” (feat. BADBADNOTGOOD)</li>
+              <li>“One Too Many” (feat. Phonte)</li>
+              <li>“Despite the Weather”</li>
+              <li>“Glowed Up” (feat. Anderson .Paak)</li>
+              <li>“Breakdance Lesson N.1”</li>
+              <li>“You're the One” (feat. Syd)</li>
+              <li>“Vivid Dreams” (feat. River Tiber)</li>
+              <li>“Lite Spots”</li>
+              <li>“Leave Me Alone” (feat. Shay Lia)</li>
+              <li>“Bullets” (feat. Little Dragon)</li>
+              <li>“Fall In Love” (feat. BADBADNOTGOOD & Shay Lia) [Bonus Track]</li>
+            </ol>
+          </details>
+        </div>
+        </section>
+
+      </div>
+    </>
+  )
+}
+
+export default App
